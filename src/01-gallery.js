914c93a119c984955f0920d5c06a93f433edd122
Krzysztof-GoIT <145707752+Krzysztof-GoIT@users.noreply.github.com>
2024-03-03 19:30:52 +0100

    HW Pull

---
+++ src/01-gallery.js
@@ -0,0 +1,22 @@
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const createGallaryItems = galleryItems
+  .map(
+    ({ original, preview, description }) =>
+      `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" data-src="${original}" alt="${description}" />
+      </a>
+    </li>`
+  )
+  .join('');
+galleryContainer.insertAdjacentHTML('beforeend', createGallaryItems);
+
+const galleryHandler = new SimpleLightbox('ul.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+galleryHandler.on('show.simplelightbox');
